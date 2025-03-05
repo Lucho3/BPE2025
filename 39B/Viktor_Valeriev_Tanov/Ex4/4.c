f256f2502161c4bb32732776c8c11da496ab8252
Victor Tanov <47223712+vichmi@users.noreply.github.com>
2025-03-05 16:15:55 +0200

    Add files via upload

--- 39B/Viktor_Valeriev_Tanov/Ex4/4.c
+++ 39B/Viktor_Valeriev_Tanov/Ex4/4.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-void makeItBinary(n) {
-    if(!n) {return;}
-    makeItBinary(n/2);
-    printf("%d", n%2);
+void recursiveBinary(int i) {
+    if(!i) {return;}
+    recursiveBinary(i/2);
+    printf("%d", i%2);
+}
+
+void makeItBinary(int num) {
+    int binary[32], i = 0;
+    while(num > 0) {
+        binary[i] = num % 2;
+        i++;
+        num /= 2;
+    }
+    for(int j=i - 1;j>=0;j--) {
+        printf("%d", binary[j]);
+    }
 }
 
 int main() {
-    makeItBinary(9);
-
+    int n = 5364;
+    recursiveBinary(n);
+    printf("\n");
+    makeItBinary(n);
     return 0;
 }
