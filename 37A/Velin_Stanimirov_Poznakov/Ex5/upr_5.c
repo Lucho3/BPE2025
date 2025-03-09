e6952908a025d355ee215a907c5aab7c860f062e
Velin Poznakov <122820392+VelinPoznakov@users.noreply.github.com>
2025-03-09 20:59:29 +0200

    Homework

---
+++ 37A/Velin_Stanimirov_Poznakov/Ex5/upr_5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // zad 1
+
+    // int *ip;
+    // double *dp;
+
+    // int number = 5;
+    // double num1 = 6.5;
+
+    // ip = &number;
+    // dp = &num1;
+
+    // printf("Address: %d\n", &number);
+    // printf("Value: %d\n", *ip);
+
+    // printf("Address: %lf\n", &num1);
+    // printf("Value: %.2f\n", *dp);
+
+    // zad 2
+
+    // int first_num, second_num;
+
+    // scanf("%d", &first_num);
+    // scanf("%d", &second_num);
+
+    // int *fnp, *snp;
+
+    // fnp = &first_num;
+    // snp = &second_num;
+
+
+    // int sum = *fnp + *snp;
+
+    // int subtract = *fnp - *snp;
+
+    // int multiply = *fnp * *snp;
+
+    // float  divide = *fnp / *snp;
+
+
+    // printf("Sum: %d\n", sum);
+
+    // printf("Subtract: %d\n", subtract);
+    
+    // printf("Multiply: %d\n", multiply);
+
+    // printf("Divide: %.2f\n", divide);
+
+    return 0;
+}
