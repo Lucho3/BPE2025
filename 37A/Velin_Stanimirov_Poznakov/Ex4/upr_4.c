5c6acc2f740c8fbe04e5253014ac220729c4da2c
Velin Poznakov <122820392+VelinPoznakov@users.noreply.github.com>
2025-03-09 20:51:26 +0200

    Homework

---
+++ 37A/Velin_Stanimirov_Poznakov/Ex4/upr_4.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// zad 1
+// float SquareArea(float a)
+// {
+//     float result = a * a;
+//     return result;
+// }
+// float RectangleArea(float a, float b)
+// {
+//     return a * b;
+// }
+// float TriangleArea(float a, float b)
+// {
+//     return (a * b) / 2;
+// }
+// float CircleArea(float r)
+// {
+//     return 3.14 * r * r;
+// }
+
+// zad 2
+
+// void SwapNumbers(int a, int b)
+// {
+//     int temp = b;
+
+//     b = a;
+//     a = temp;
+
+//     printf("A is: ");
+//     printf("%d\n", a);
+
+//     printf("B is: ");
+//     printf("%d", b);
+// }
+
+// zad 3
+
+// int EvenOrOdd(int a)
+// {
+//     if(a % 2 == 0)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+// zad 4
+
+// void NumberTransform(int a)
+// {
+//     int arr[32];
+//     int i = 0;
+//     while (a > 0) {
+//         arr[i] = a % 2;
+//         a /= 2;
+//         i++;
+//     }
+
+//      for (int k = i - 1; k >= 0; k--)
+//     {
+//         printf("%d", arr[k]);
+//     }
+// }
+
+int main()
+{
+    // zad 1
+
+    // int number;
+
+    // scanf("%d", &number);
+
+    // if(number == 1)
+    // {
+    //     float a;
+    //     float area;
+
+    //     scanf("%f", &a);
+
+    //     area = SquareArea(a);
+
+    //     printf("Square: ");
+    //     printf("%f", area);
+    // }
+    //  else if(number == 2)
+    // {
+    //     float a, b;
+
+    //     scanf("%f", &a);
+    //     scanf("%f", &b);
+
+    //     float area = RectangleArea(a, b);
+
+    //     printf("Rectangle: ");
+    //     printf("%f", area);
+    // }
+    // else if(number == 3)
+    // {
+    //     float a, b;
+
+    //     scanf("%f", &a);
+    //     scanf("%f", &b);
+
+    //     float area = TriangleArea(a, b);
+
+    //     printf("Triangle: ");
+    //     printf("%f", area);
+    // }
+    // else if(number == 4)
+    // {
+    //     float r;
+
+    //     scanf("%f", &r);
+
+    //     float area = CircleArea(r);
+
+    //     printf("%f", area);
+    // }
+
+    // zad 2
+
+    // int num1, num2;
+
+    // scanf("%d", &num1);
+    // scanf("%d", &num2);
+
+    // SwapNumbers(num1, num2);
+
+    // zad 3
+
+    // int a;
+
+    // printf("Enter a: ");
+    // scanf("%d", &a);
+
+    // int result = EvenOrOdd(a);
+
+    // printf("%d", result);
+
+    // zad 4
+
+    // int a;
+
+    // scanf("%d", &a);
+
+    // NumberTransform(a);
+
+    return 0;
+}
