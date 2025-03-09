6d6701f3ece05309860f4d44988e0aeda05c20ff
Maftzer <138110735+Maftzer@users.noreply.github.com>
2025-03-09 19:02:08 +0200

    Update zad2.c

--- 39B/Danail_Dimitrov_Atanasov/Ex4/zad2.c
+++ 39B/Danail_Dimitrov_Atanasov/Ex4/zad2.c
@@ -1,19 +1,36 @@
+// #include <stdio.h>
+// int Switch(int a, int b);
+// int main(){
+//     int a;
+//     int b;
+//     printf("Enter two numbers to be switched: ");
+//     scanf("%d %d", &a, &b);
+//     a,b = Switch(a,b);
+//     printf("The numbers now are a: %d and b: %d", a, b);
+// }
+
+// int Switch(int a, int b){
+//     a = a+b;
+//     b = a-b;
+//     a = a-b;
+//     return a,b
+
+// }
+
 #include <stdio.h>
-int Switch(int a, int b);
-int main(){
-    int a;
-    int b;
+void Switch(int arr[2]);
+int main() {
+    int arr[2];
     printf("Enter two numbers to be switched: ");
-    scanf("%d %d", &a, &b);
-    a,b = Switch(a,b);
-    printf("The numbers now are a: %d and b: %d", a,b);
+    scanf("%d %d", &arr[0], &arr[1]);
+    
+    Switch(arr);
+    printf("The numbers now are a: %d and b: %d\n", arr[0], arr[1]);
 }
 
-int Switch(int a, int b){
-    a = a+b;
-    b = a-b;
-    a = a-b;
-    return a,b;
-
+void Switch(int arr[2]) {
+    arr[0] = arr[0] + arr[1];
+    arr[1] = arr[0] - arr[1];
+    arr[0] = arr[0] - arr[1];
 }
 
